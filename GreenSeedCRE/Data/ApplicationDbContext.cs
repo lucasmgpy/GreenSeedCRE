590bf6f7726aed9591154868ed1c654b8fbb25c2
lucasmgpy <lucasmg.py@gmail.com>
2024-09-27 18:12:35 +0100

    Update ApplicationDbContext.cs

--- GreenSeedCRE/Data/ApplicationDbContext.cs
+++ GreenSeedCRE/Data/ApplicationDbContext.cs
@@ -1,13 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using GreenSeedCRE.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GreenSeedCRE.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Challenge> Challenges { get; set; }
+        public DbSet<ChallengeOp> ChallengeOps  { get; set; }
+
+       
     }
 }
