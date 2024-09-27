e28c7fc63d1d78444754ae5341462857d209225a
lucasmgpy <lucasmg.py@gmail.com>
2024-09-27 18:15:02 +0100

    apagando tudo
    apagando tudo

--- GreenSeedCRE/Models/ChallengeOp.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GreenSeedCRE.Models
-{
-    public class ChallengeOp
-    {
-        public int ChallengeOpId { get; set; }
-        public int ChallengeId { get; set; }
-        public string OpName { get; set; }
-    }
-}
