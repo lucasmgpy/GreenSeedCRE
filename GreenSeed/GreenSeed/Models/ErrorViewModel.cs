f60d93e58d887aeaf9b2e1aff91c3713a452e218
lucasmgpy <lucasmg.py@gmail.com>
2024-10-07 18:51:20 +0100

    delete all

--- GreenSeed/GreenSeed/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GreenSeed.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
