d0f999c19b74f9f631b6f35da1853ae4327c47f2
lucasmgpy <lucasmg.py@gmail.com>
2024-10-25 22:15:47 +0100

    Azure SQL Database
    alteração de sql local para Azure sql

--- GreenSeed.Tests/obj/Debug/net8.0/GreenSeed.Tests.AssemblyInfo.cs
+++ GreenSeed.Tests/obj/Debug/net8.0/GreenSeed.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenSeed.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61956f701a080c22ceb5f5a1a9dd849f12c0b3af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f17e7febe22b9778f40e0aca1d466aea9d7463")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenSeed.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenSeed.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
